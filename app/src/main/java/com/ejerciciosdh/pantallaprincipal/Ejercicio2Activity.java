77e1e5cc93257a6a5597ed8dff8cd315f4c4853d
Unknown <julian.misiak@gmail.com>
2018-09-12 16:16:22 -0300

    Ejercicios 1, 2 y 3

---
+++ app/src/main/java/com/ejerciciosdh/pantallaprincipal/Ejercicio2Activity.java
@@ -0,0 +1,13 @@
+package com.ejerciciosdh.pantallaprincipal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ejercicio2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio2);
+    }
+}
---
+++ app/src/main/java/com/ejerciciosdh/pantallaprincipal/Ejercicio3Activity.java
@@ -0,0 +1,13 @@
+package com.ejerciciosdh.pantallaprincipal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ejercicio3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ejercicio3_activity);
+    }
+}
