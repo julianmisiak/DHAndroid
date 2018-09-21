ee70546db7f74c6a32e116db12fea7c62f3faa6e
Unknown <julian.misiak@gmail.com>
2018-09-21 20:34:46 -0300

    Reloj
    Falta terminar

---
+++ app/src/main/java/com/ejerciciosdh/pantallaprincipal/RelojActivity.java
@@ -0,0 +1,13 @@
+package com.ejerciciosdh.pantallaprincipalactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelojActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reloj_activity);
+    }
+}
---
+++ app/src/main/java/com/ejerciciosdh/pantallaprincipalactivity/RelojActivity2.java
@@ -0,0 +1,13 @@
+package com.ejerciciosdh.pantallaprincipalactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelojActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reloj_activity2);
+    }
+}
