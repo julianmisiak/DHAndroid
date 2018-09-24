0f7472f701895f58c981a94ed6f028be1e959f90
Unknown <julian.misiak@gmail.com>
2018-09-24 12:09:47 -0300

    Fragment

---
+++ app/src/main/java/com/ejerciciosdh/pantallaprincipal/FragmentsEjerActivity.java
@@ -0,0 +1,13 @@
+package com.ejerciciosdh.pantallaprincipal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FragmentsEjerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragments_ejer);
+    }
+}
---
+++ app/src/main/java/com/ejerciciosdh/pantallaprincipal/RojoFragment.java
@@ -0,0 +1,32 @@
+package com.ejerciciosdh.pantallaprincipal;
+
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ejerciciosdh.pantallaprincipalactivity.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class RojoFragment extends Fragment {
+
+
+    public RojoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_rojo, container, false);
+    }
+
+}
